908c1d64e4e4d6d2db99160bd299b4c5bfdc2f5f
Jose Rivera <jrivera@newrelic.com>
2017-02-06 19:33:27 -0800

    Initial commit.

--- PowerMate.py
+++ PowerMate.py
@@ -1,33 +1,35 @@
 import sublime, sublime_plugin
 
+# Takes the focus to the last Tab in the current Group.
 class PowerLastTabCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        window = self.window
-        views = window.views_in_group(window.active_group())
-        if len(views) >= 9:
-          window.focus_view(views[9])
-        else:
-          window.focus_view(views[-1])
+  def run(self):
+    window = self.window
+    views = window.views_in_group(window.active_group())
+    window.focus_view(views[len(views) - 1])
 
+# Takes the focus to the first Tab in the current Group.
 class PowerFirstTabCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        window = self.window
-        views = window.views_in_group(window.active_group())
-        window.focus_view(views[0])
+  def run(self):
+    window = self.window
+    views = window.views_in_group(window.active_group())
+    window.focus_view(views[0])
 
+# Takes the focus to the next Tab in the current Group.
 class PowerNextTabCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        window = self.window
-        views = window.views_in_group(window.active_group())
-        view = window.get_view_index(self.window.active_view)
-        view = view + 1
-        if view < views:
-          window.focus_view(views[view])
+  def run(self):
+    window = self.window
+    views = window.views_in_group(window.active_group())
+    group, view_index = window.get_view_index(window.active_view())
+    view_index += 1
+    if view_index < len(views):
+      window.focus_view(views[view_index])
 
+# Takes the focus to the previous Tab in the current Group.
 class PowerPreviousTabCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        window = self.window
-        view = window.get_view_index(self.window.active_view)
-        view = view - 1
-        if view > 0:
-          window.focus_view(views[view])
+  def run(self):
+    window = self.window
+    views = window.views_in_group(window.active_group())
+    group, view_index = window.get_view_index(window.active_view())
+    view_index -= 1
+    if view_index >= 0:
+      window.focus_view(views[view_index])
---
+++ README.md
@@ -0,0 +1,7 @@
+# PowerMate-Sublime-Plugin
+## Summary
+I started using Griffin Powermate with Sublime to navigate thru all tabs in my development workflow. Found that is annoying when you navigate too quickly and circle back on the other side of the screen, I wanted to be able to twist the PowerMate knob quickly without having to worry about it going all the way to the other side of the screen. 
+
+So I went and built this, as my first attempt to Python. Now I can just map PowerMate's function to what the keybindings are and it will stop at either side of the Tab bar. 
+
+That's all there is.
